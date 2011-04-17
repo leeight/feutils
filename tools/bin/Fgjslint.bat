291eaef8b453465ce1f9d8cb818753d8d2ba98e5
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-04-17 07:28:31 +0000

    修复一下bat文件的问题
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@71477 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fgjslint.bat
+++ tools/bin/Fgjslint.bat
@@ -3,5 +3,5 @@ setlocal
 :: This is required with cygwin only.
 set PATH=%~dp0;%PATH%
 :: This is required with python module search
-set PYTHONPATH="%~dp0..\lib\google-closure-linter"
+set PYTHONPATH=%~dp0..\lib\google-closure-linter
 call python "%~dp0..\lib\google-closure-linter\closure_linter\gjslint.py" %*
--- tools/bin/gjslint.bat
+++ tools/bin/gjslint.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 :: This is required with cygwin only.
-PATH=%~dp0;%PATH%
+set PATH=%~dp0;%PATH%
 :: This is required with python module search
-PYTHONPATH="%~dp0..\lib\google-closure-linter"
+set PYTHONPATH=%~dp0..\lib\google-closure-linter
 call python "%~dp0..\lib\google-closure-linter\closure_linter\gjslint.py" %*
