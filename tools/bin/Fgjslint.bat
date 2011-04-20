817b24cb4c2dc8e9c7b56e1cbb76591c6bee43c8
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-04-20 15:43:52 +0000

    update tools/bin
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@71914 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fgjslint.bat
+++ tools/bin/Fgjslint.bat
@@ -4,4 +4,4 @@ setlocal
 set PATH=%~dp0;%PATH%
 :: This is required with python module search
 set PYTHONPATH=%~dp0..\lib\google-closure-linter
-call python "%~dp0..\lib\google-closure-linter\closure_linter\gjslint.py" %*
+call python -S "%~dp0..\lib\google-closure-linter\closure_linter\gjslint.py" %*
