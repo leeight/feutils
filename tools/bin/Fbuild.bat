d6000de7876dfd4b17d3b1c1c0b93357ef16cf94
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-15 12:40:19 +0000

    add api
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81181 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fbuild.bat
+++ tools/bin/Fbuild.bat
@@ -4,7 +4,7 @@ setlocal
 set PATH=%~dp0;%PATH%
 set NODE_PATH=%NODE_PATH%;%~dp0..\lib
 
-IF NOT EXIST C:\WIN\NUL GOTO NOBUILDFILE
+IF NOT EXIST "build.js" GOTO NOBUILDFILE
 call node "build.js" %*
 
 :NOBUILDFILE
