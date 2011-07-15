10a9652352125e7785887bb8899c3ba803487512
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-15 12:42:07 +0000

    add api
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81183 01db0846-df26-4232-8c27-33e78907c97d

--- tools/lib/Fbuild.js
+++ tools/lib/Fbuild.js
@@ -15,9 +15,55 @@
  *
  **/
 
-var HELLO_WORLD = "CLEAR";
+var util  = require('util'),
+    fs    = require('fs'),
+    spawn = require('child_process').spawn;
 
-exports.HELLO_WORLD = HELLO_WORLD;
+/**
+ * @param {string} input 归一化文件的路径.
+ * @return {string} 归一化之后的路径.
+ */
+function getPath(input) {
+  if (process.platform == 'win32') {
+    return input.replace(/\//g, '\\');
+  } else {
+    return input;
+  }
+}
+
+/**
+ * @param {string} command 要执行的命令.
+ * @param {Array.<string>=} opt_args 命令参数，可选.
+ * @param {string=} opt_output 日志输出的文件，可选.
+ */
+function exec(command, opt_args, opt_output) {
+  var task = spawn(getPath(command), opt_args || []);
+
+  var output = [];
+  if (opt_output) {
+    task.stdout.on('data', function (data) {
+      output.push(data);
+    });
+  }
+
+  task.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  task.on('exit', function (code) {
+    if (code == 0) {
+      if (opt_output) {
+        fs.writeFile(opt_output, output.join(''));
+      }
+    } else {
+      console.log('child process exited with code ' + code);
+    }
+  });
+}
+
+
+exports.exec = exec;
+exports.getPath = getPath;
 
 
 
