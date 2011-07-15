a7ecc2b2119bb6803fcdcdb8c39a562961d417ff
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-15 11:32:50 +0000

    add Fbuild
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81169 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fbuild.bat
+++ tools/bin/Fbuild.bat
@@ -2,4 +2,9 @@
 setlocal
 :: This is required with cygwin only.
 PATH=%~dp0;%PATH%
+
+IF NOT EXIST C:\WIN\NUL GOTO NOBUILDFILE
 call node "Fbuild.js" %*
+
+:NOBUILDFILE
+echo "Can't find Fbuild.js in current directory"
