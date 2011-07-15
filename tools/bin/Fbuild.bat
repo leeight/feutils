e55e376028718f638fa924452764874b250007c5
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-15 11:54:12 +0000

    更新Fbuild.bat
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81173 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fbuild.bat
+++ tools/bin/Fbuild.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 :: This is required with cygwin only.
-PATH=%~dp0;%PATH%
-NODE_PATH=%NODE_PATH%;%~dp0..\lib
+set PATH=%~dp0;%PATH%
+set NODE_PATH=%NODE_PATH%;%~dp0..\lib
 
 IF NOT EXIST C:\WIN\NUL GOTO NOBUILDFILE
 call node "build.js" %*
