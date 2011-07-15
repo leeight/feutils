a93d4f544ed7caf294efe740092d9720249ec499
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-15 11:52:55 +0000

    add api
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81172 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fbuild.bat
+++ tools/bin/Fbuild.bat
@@ -2,9 +2,10 @@
 setlocal
 :: This is required with cygwin only.
 PATH=%~dp0;%PATH%
+NODE_PATH=%NODE_PATH%;%~dp0..\lib
 
 IF NOT EXIST C:\WIN\NUL GOTO NOBUILDFILE
-call node "Fbuild.js" %*
+call node "build.js" %*
 
 :NOBUILDFILE
-echo Can't find Fbuild.js in current directory
+echo Can't find build.js in current directory
---
+++ tools/lib/Fbuild.js
@@ -0,0 +1,40 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2011 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * ../lib/Fbuild.js ~ 2011/07/15 19:45:52
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *
+ **/
+
+var HELLO_WORLD = "CLEAR";
+
+exports.HELLO_WORLD = HELLO_WORLD;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
