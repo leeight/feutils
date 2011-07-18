4ec910f9eda9e8afd8d3a523117f021bc96c09ab
leeight <leeight@gmail.com>
2011-07-18 13:41:43 +0800

    init repos

---
+++ bin/Fbuild.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+:: This is required with cygwin only.
+set PATH=%~dp0;%PATH%
+set NODE_PATH=%NODE_PATH%;%~dp0..\lib
+
+IF NOT EXIST "build.js" GOTO NOBUILDFILE
+call node "build.js" %*
+
+:NOBUILDFILE
+echo Can't find build.js in current directory
---
+++ bin/Fhelp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#!-*- coding:utf-8 -*-
+#!vim: set ts=4 sw=4 sts=4 tw=100 noet:
+# ***************************************************************************
+# 
+# Copyright (c) 2011 Baidu.com, Inc. All Rights Reserved
+# $Id$ 
+# 
+# **************************************************************************/
+ 
+ 
+ 
+import os
+import sys
+ 
+ 
+__author__ = 'leeight <liyubei@baidu.com>'
+__date__ = '2011/04/17 10:39:46'
+__revision = '$Revision$'
+
+FHELP_PY_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+  commands = []
+  for entry in os.listdir(FHELP_PY_PATH):
+    if os.path.isfile(os.path.join(FHELP_PY_PATH, entry)):
+      if entry.startswith('F'):
+        if entry.find(".") == -1:
+          commands.append(entry)
+
+  print "Available commands:"
+  for command in commands:
+    print "  %s" % command
+
+if __name__ == "__main__":
+  main()
+
+
+
