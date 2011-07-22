0764f0347fda240ecb009860f64bb5bc138f91f0
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-22 06:13:29 +0000

    add Funinstall.bat
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81931 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Funinstall.bat
+++ tools/bin/Funinstall.bat
@@ -3,9 +3,11 @@
 pushd "%~dp0"
 
 :: Install Fzip
+echo Uninstalling Fzip...
 rundll32 setupapi.dll,InstallHinfSection DefaultUnInstall 128 .\Fzip.install.inf
 
 :: Install Fformat
+echo Uninstalling Fformat...
 :: rundll32 setupapi.dll,InstallHinfSection DefaultUnInstall 128 .\Fformat.install.inf
 
 popd
