783472891d40a35f3c2487663035d83f016eea8f
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-07-22 04:28:46 +0000

    Fformat支持json和js了
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@81904 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fhelp.py
+++ tools/bin/Fhelp.py
@@ -20,13 +20,30 @@ __revision = '$Revision$'
 
 FHELP_PY_PATH = os.path.dirname(os.path.abspath(__file__))
 
+def which(program):
+  def is_exe(fpath):
+    return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+  fpath, fname = os.path.split(program)
+  if fpath:
+    if is_exe(program):
+      return program
+  else:
+    for path in os.environ["PATH"].split(os.pathsep):
+      exe_file = os.path.join(path, program)
+      if is_exe(exe_file):
+        return exe_file
+
+  return None
+
 def main():
   commands = []
   for entry in os.listdir(FHELP_PY_PATH):
     if os.path.isfile(os.path.join(FHELP_PY_PATH, entry)):
       if entry.startswith('F'):
         if entry.find(".") == -1:
-          commands.append(entry)
+          if which(entry):
+            commands.append(entry)
 
   print "Available commands:"
   for command in commands:
