585036b5e762e48abc3a4ac147d5cb5413b6903b
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-08-19 06:39:42 +0000

    add Ftangram
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@88306 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Ftangram.bat
+++ tools/bin/Ftangram.bat
@@ -4,4 +4,4 @@ setlocal
 set PATH=%~dp0;%PATH%
 :: This is required with python module search
 set PYTHONPATH=%~dp0..\lib\google-closure-linter
-call python -S "%~dp0Ftamgram.py" %*
+call python -S "%~dp0Ftangram.py" %*
