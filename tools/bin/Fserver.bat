14500f746bd47985a46979d33f98cd67a0ed387e
(no author) <(no author)@01db0846-df26-4232-8c27-33e78907c97d>
2011-10-20 10:52:02 +0000

    修改windows下面的一些错误
    git-svn-id: http://fe.baidu.com/repos/doc/sdcfe@93227 01db0846-df26-4232-8c27-33e78907c97d

--- tools/bin/Fserver.bat
+++ tools/bin/Fserver.bat
@@ -4,4 +4,14 @@ setlocal
 set PATH=%~dp0;%PATH%
 :: This is required with python module search
 set NODE_PATH=%~dp0..\lib\libproxy\node_modules
+SET /A ARGS_COUNT=0
+FOR %%A in (%*) DO SET /A ARGS_COUNT+=1
+IF %ARGS_COUNT%==0 GOTO :help
 call node %*
+GOTO :exit
+
+:help
+echo Usage: Fserver server.js [options]
+
+:exit
+
